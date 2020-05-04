2452ac1e3c0e27609510a51a978e7b10c2e7a47d
doodeck <8515450+doodeck@users.noreply.github.com>
2020-05-04 15:11:52 -0300

    rudimentary connection to redis works

--- app/index.js
+++ app/index.js
@@ -1,11 +1,31 @@
 var express = require('express');
 var os = require("os");
+const redis = require("redis");
+const host = process.env.REDIS || '127.0.0.1'
+/*
+ * To start redis locally, for development:
+ * sudo docker service create --name redis --network skynet -p 6379:6379 redis:alpine@sha256:cfba805e8e322ce58dab2b9b4b858f37f7302e0e2f6cda93544aea5f6b65672a
+ */
+const client = redis.createClient(6379, host);
+const counter_key = 'counter';
+
+client.on("error", function(error) {
+  console.error(error);
+});
+
+client.on('connect', function() {
+  console.log('connected');
+  client.set(counter_key, 0);
+});
 
 var hostname = os.hostname();
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! Running on "' + hostname + '"');
+  client.get(counter_key, function(err, reply) {
+    client.set(counter_key, 1 + parseInt(reply));
+    res.send('Hello World! Running on "' + hostname + '" / redis count: ' + reply);
+  });
 });
 
 app.listen(3000, function () {
