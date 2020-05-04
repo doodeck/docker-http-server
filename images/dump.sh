c8d1fce53ba3d6dc398fd5cfe570942b046e1f39
doodeck <8515450+doodeck@users.noreply.github.com>
2020-05-04 11:24:57 -0300

    Saved an image

---
+++ images/dump.sh
@@ -0,0 +1,4 @@
+#
+
+sudo docker pull node:14.1.0-buster-slim@sha256:c4222a65b0ef1ffad568b684f2c92918613c52c872c32efedc873410946a70f1
+sudo docker save node:14.1.0-buster-slim@sha256:c4222a65b0ef1ffad568b684f2c92918613c52c872c32efedc873410946a70f1 -o node_14_1-buster-slim.tar
