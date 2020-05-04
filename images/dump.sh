95d9f43a36a5326ce72dff5b0cac4d64359dbf8d
doodeck <8515450+doodeck@users.noreply.github.com>
2020-05-04 11:43:09 -0300

    ppc64le registry uploaded

--- images/dump.sh
+++ images/dump.sh
@@ -1,4 +1,9 @@
 #
 
+sudo docker pull ibmcom/registry-ppc64le:2.7.1
+sudo docker save ibmcom/registry-ppc64le:2.7.1 -o ibmcom_registry-ppc64le_2_7_1.tar
+sudo zip -9 ibmcom_registry-ppc64le_2_7_1.zip ibmcom_registry-ppc64le_2_7_1.tar
+
 sudo docker pull node:14.1.0-buster-slim@sha256:c4222a65b0ef1ffad568b684f2c92918613c52c872c32efedc873410946a70f1
 sudo docker save node:14.1.0-buster-slim@sha256:c4222a65b0ef1ffad568b684f2c92918613c52c872c32efedc873410946a70f1 -o node_14_1-buster-slim.tar
+sudo zip -9 node_14_1-buster-slim.zip node_14_1-buster-slim.tar
