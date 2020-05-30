653f4b5d84a0668ace2fcb038679e0e73746b153
doodeck <8515450+doodeck@users.noreply.github.com>
2020-05-30 06:35:17 +0200

    Update dump.sh

--- images/dump.sh
+++ images/dump.sh
@@ -7,3 +7,7 @@ sudo zip -9 ibmcom_registry-ppc64le_2_7_1.zip ibmcom_registry-ppc64le_2_7_1.tar
 sudo docker pull node:14.1.0-buster-slim@sha256:c4222a65b0ef1ffad568b684f2c92918613c52c872c32efedc873410946a70f1
 sudo docker save node:14.1.0-buster-slim@sha256:c4222a65b0ef1ffad568b684f2c92918613c52c872c32efedc873410946a70f1 -o node_14_1-buster-slim.tar
 sudo zip -9 node_14_1-buster-slim.zip node_14_1-buster-slim.tar
+
+# both images ibmcom_registry-ppc64le_2_7_1.zip and node_14_1-buster-slim.zip kept here in the folder but deleted
+# still to be found in history
+
