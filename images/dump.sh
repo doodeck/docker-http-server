349786a91e1e530520b71f12482b080ed888c453
doodeck <8515450+doodeck@users.noreply.github.com>
2020-07-15 15:08:09 +0200

    Further, recent ppc64le images

--- images/dump.sh
+++ images/dump.sh
@@ -16,6 +16,17 @@ sudo docker pull node:10.13.0-alpine@sha256:bf89495913ed5dcbdbaa5868ebf2624b00d3
 sudo docker save node:10.13.0-alpine@sha256:bf89495913ed5dcbdbaa5868ebf2624b00d3cbae605769ca2cc164b08c207e3f -o node_10_13_0-alpine.tar
 sudo zip -9 node_10_13_0-alpine.zip node_10_13_0-alpine.tar
 
+sudo docker pull node:14.5.0@sha256:f0be9252ba0451e2125f0f5f9f7c3a69f788b45fd6b748e0f7e2a4a55dfd11e6
+sudo docker image tag 027354bff17a node:14.5.0
+sudo docker save node:14.5.0@sha256:f0be9252ba0451e2125f0f5f9f7c3a69f788b45fd6b748e0f7e2a4a55dfd11e6 -o node_14_5_0.tar
+sudo zip -9 node_14_5_0.zip node_14_5_0.tar
+
+# analog
+sudo docker pull node:12.18.2@sha256:8ac24e61704f0cc39fc0eae731e99238d337b9ff496ed48cf435fe7dc7f998ff
+sudo docker pull node:12.18.2-stretch-slim@sha256:301b58626afec806d3caa7063e94352aa685b4451ef505e0f520aecf94e71af9
+sudo docker pull node:12.18.2-alpine3.11@sha256:10975d93ed3c5701f40b8481e1e6e9f30d7cd91a3b957586d3526a4a83adecee
+# ...
+
 # both images ibmcom_registry-ppc64le_2_7_1.zip and node_14_1-buster-slim.zip kept here in the folder but deleted
 # still to be found in history
 
